8f47f53730a0480b2304da48ff518489b313c8e3
孤鸿 夜 <litiansum@foxmail.com>
2017-09-17 19:03:14 +0800

    修改编辑器高度

--- src/ABPBlog.Web.Mvc/wwwroot/lib/ckeditor/config.js
+++ src/ABPBlog.Web.Mvc/wwwroot/lib/ckeditor/config.js
@@ -26,7 +26,7 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'colors' },
 		{ name: 'about' }
 	];
-
+    config.height = 500; //¸ß¶È
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
 	config.removeButtons = 'Underline,Subscript,Superscript';
